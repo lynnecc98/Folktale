0b4a6fc5bc36d707c70838a78cdcea4bbec6801b
Chloe Choi <lynnecc98@gmail.com>
2020-10-19 00:31:26 +0900

    Finished scene 2 with interactions. Need to touch up timing

---
+++ Assets/TimelineManager.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+using UnityEngine.Timeline;
+
+public class TimelineManager : MonoBehaviour
+{
+    public PlayableDirector Timeline1;
+    public PlayableDirector Timeline2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Timeline1.time > 6.5) 
+        {
+            if (Input.GetKeyDown("space")) {
+                Timeline1.Stop();
+                Timeline2.Play();
+            }
+        }
+    }
+
+
+}
