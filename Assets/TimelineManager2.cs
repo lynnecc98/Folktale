5cfa32539222ab204f4e804b1bd11e5eff016df1
Chloe Choi <lynnecc98@gmail.com>
2020-10-19 16:24:28 +0900

    finished timeline for scene 3. Need to decide and add interaction

---
+++ Assets/TimelineManager2.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+using UnityEngine.Timeline;
+
+public class TimelineManager2 : MonoBehaviour
+{
+    public PlayableDirector Timeline1;
+    public PlayableDirector Timeline2;
+    public GameObject FadeOut;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Timeline1.time > 6)
+        {
+            if (Input.GetKeyDown("space"))
+            {
+                Timeline1.Stop();
+                Timeline2.Play();
+            }
+        }
+        if (Timeline2.time > 6)
+        {
+            FadeOut.SetActive(true);
+        }
+    }
+
+
+}
