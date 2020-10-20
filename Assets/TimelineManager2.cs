95cfcf9692397ad3126b9b194babb72766c0868d
Chloe Choi <lynnecc98@gmail.com>
2020-10-20 09:42:16 +0900

    Finished be quiet scene

--- Assets/TimelineManager2.cs
+++ Assets/TimelineManager2.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Animations;
 using UnityEngine;
 using UnityEngine.Playables;
 using UnityEngine.Timeline;
@@ -10,6 +11,8 @@ public class TimelineManager2 : MonoBehaviour
     public PlayableDirector Timeline1;
     public PlayableDirector Timeline2;
     public GameObject FadeOut;
+    public Animator son;
+    bool paused = false;
 
     // Start is called before the first frame update
     void Start()
@@ -20,13 +23,25 @@ public class TimelineManager2 : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Timeline1.time > 6)
+        if (Timeline1.time > 9.3)
         {
-            if (Input.GetKeyDown("space"))
+            if (paused == false) 
             {
-                Timeline1.Stop();
-                Timeline2.Play();
+                Timeline1.Pause();
+                paused = true;
+
             }
+            
+        }
+        if (paused == true && Input.GetKeyDown("space")) 
+        {
+            Timeline1.Resume();
+            son.SetBool("isWalking", true);
+        }
+        if (Timeline1.time > 12.1) 
+        {
+            Timeline1.Stop();
+            Timeline2.Play();
         }
         if (Timeline2.time > 6)
         {
